43514eb4c780af0731b55f9df03f00cf0e991fc2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-05 00:53:03 +0300

    Added empty class Hard

---
+++ src/tictactoe/ai/Hard.java
@@ -0,0 +1,10 @@
+package tictactoe.ai;
+
+import tictactoe.engine.Board;
+import tictactoe.engine.Mark;
+
+public class Hard extends Medium {
+    public Hard(Board board, Mark mark) {
+        super(board, mark);
+    }
+}
